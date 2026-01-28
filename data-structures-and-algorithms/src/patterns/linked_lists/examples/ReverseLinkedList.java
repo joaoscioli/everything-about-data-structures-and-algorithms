b26811a5965ac8defbb056dca3ef4393c9486387
joaoscioli <joaoscioli@outlook.com>
2026-01-28 20:51:51 -0300

    feat(linked lists) add ReverseLinkedList

--- data-structures-and-algorithms/src/patterns/linked_lists/examples/ReverseLinkedList.java
+++ data-structures-and-algorithms/src/patterns/linked_lists/examples/ReverseLinkedList.java
@@ -3,6 +3,11 @@ package patterns.linked_lists.examples;
 /// Reverse Linked List
 /// Given the head of a singly linked list, reverse the list and return the new head.
 ///
+/// **Complexity Analysis**
+/// **Time complexity:** The time complexity of linked_list_reversal is O(n), where n denotes the
+/// length of the linked list This is because we perform constant-time pointer manipulation at each
+/// node of the linked list
+/// **Space complexity:** The space complexity is 0(1).
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
