0911cbbc5252fbe9f93db7df347e3680ad9054fd
joaoscioli <joaoscioli@outlook.com>
2026-02-10 14:59:07 -0300

    feat(fast slow pointers) add new project ListNode

---
+++ data-structures-and-algorithms/src/patterns/fast_slow_pointers/examples/ListNode.java
@@ -0,0 +1,11 @@
+package patterns.fast_slow_pointers.examples;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
